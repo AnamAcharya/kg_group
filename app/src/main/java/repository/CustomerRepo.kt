455ddd1896201d80d40e8d8c291493d2f515a222
unknown <anamacharya1@gmail.com>
2021-12-20 14:04:47 +0545

    first commit of KG group

---
+++ app/src/main/java/repository/CustomerRepo.kt
@@ -0,0 +1,23 @@
+package repository
+
+import api.ApiRequest
+import api.CustomerAPI
+import api.ServiceBuilder
+import entity.Customer
+import response.UserResponse
+
+class CustomerRepo:ApiRequest() {
+    val myApi = ServiceBuilder.buildService(CustomerAPI::class.java)
+
+    suspend fun registerUser(customer: Customer): UserResponse {
+        return apiRequest {
+            myApi.registerUser(customer)
+        }
+    }
+
+    suspend fun checkUser(phone: String, password: String): UserResponse {
+        return apiRequest {
+            myApi.checkUser(phone, password)
+        }
+    }
+}
